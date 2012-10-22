7650e5d9a3b82f0b1b615646dd11270b03105571
richard <richard@jarofdoom.co.uk>
2012-10-22 19:31:57 +0000

    Pycache from python 2 branch not excepted. Reverting r105 because old code -.-'

--- highs.py
+++ highs.py
@@ -44,10 +44,12 @@ class HighScore:
                 self.highEnd = Config.GAME_CODE_USER_END
             elif(event.type == pygame.QUIT):
                 self.highEnd = Config.GAME_CODE_USER_END
+                break
             elif(event.type == pygame.KEYDOWN):
                 if(event.key == pygame.K_ESCAPE):
                     self.highEnd = Config.GAME_CODE_USER_END
-         
+                    break
+ 
     def start(self,score):   
         self.highEnd = 0            
         clock = pygame.time.Clock()
@@ -125,7 +127,6 @@ class HighScore:
             self.__handleUserInteraction()
             html.write(self.screen,self.font,pygame.Rect(300,25,700,700),data)
             pygame.display.flip()
-        pygame.event.clear()
         return self.highEnd
 
 if __name__ == '__main__':
@@ -135,7 +136,6 @@ if __name__ == '__main__':
 
     game_scores = HighScore(screen)
     game_scores.start(470)
-
 
 
 
